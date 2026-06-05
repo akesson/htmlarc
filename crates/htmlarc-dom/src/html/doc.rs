57a25026fd68a6599207ccfabcbf0ce68a842043
Henrik <henrik@akesson.mobi>
2026-06-05 19:59:21 +0200

    Remove unused document serialization helpers

--- crates/htmlarc-dom/src/html/doc.rs
+++ crates/htmlarc-dom/src/html/doc.rs
@@ -3,7 +3,6 @@ use crate::{
     parser::{Chars, DomBuilder, DomBuilderCursor, parse_doc},
     prelude::*,
 };
-use rkyv::{rancor::Error, util::AlignedVec};
 use std::cell::RefCell;
 pub struct HtmlDoc {
     pub(crate) dom: DomInner,
@@ -38,23 +37,6 @@ impl HtmlDoc {
         parse_doc(&mut dom, &mut chars)?;
         Ok(dom.to_string())
     }
-    pub fn to_bytes(&self) -> AlignedVec {
-        rkyv::to_bytes::<Error>(&self.dom).unwrap()
-    }
-
-    pub fn from_bytes(bytes: &[u8]) -> Self {
-        unsafe {
-            rkyv::from_bytes_unchecked::<DomInner, Error>(bytes)
-                .unwrap()
-                .into()
-        }
-    }
-
-    pub fn write_to(&self, path: &std::path::Path) {
-        let data = self.to_bytes();
-        std::fs::write(path, data).unwrap();
-    }
-
     pub fn to_html(&self, fmt: HtmlFormat) -> String {
         self.dom.to_html(fmt)
     }
